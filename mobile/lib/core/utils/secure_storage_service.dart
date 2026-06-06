a7004dea93587c0a02d0ace91e401de4d12aec47
jaswanth4237 <24p35a4237@acet.ac.in>
2026-06-06 19:44:33 +0530

    feat: secure storage service

---
+++ mobile/lib/core/utils/secure_storage_service.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:injectable/injectable.dart';
+
+@singleton
+class SecureStorageService {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  Future<void> saveAccessToken(String token) async {
+    await _storage.write(key: 'access_token', value: token);
+  }
+
+  Future<void> saveRefreshToken(String token) async {
+    await _storage.write(key: 'refresh_token', value: token);
+  }
+
+  Future<String?> getAccessToken() async {
+    return await _storage.read(key: 'access_token');
+  }
+
+  Future<String?> getRefreshToken() async {
+    return await _storage.read(key: 'refresh_token');
+  }
+
+  Future<void> clearAll() async {
+    await _storage.deleteAll();
+  }
+
+  Future<void> saveString(String key, String value) async {
+    await _storage.write(key: key, value: value);
+  }
+
+  Future<String?> getString(String key) async {
+    return await _storage.read(key: key);
+  }
+}
